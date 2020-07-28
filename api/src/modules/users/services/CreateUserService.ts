e04802c40930c8837c777d59259bda8664de6807
Michel Rodrigues <sobreira.michel@gmail.com>
2020-07-28 18:54:01 -0300

    API: apply solid pattern design and ddd

---
+++ api/src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,41 @@
+import { hash } from 'bcryptjs';
+
+import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import { injectable, inject } from 'tsyringe';
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ email, name, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const hashPassword = await hash(password, 8);
+
+    const user = this.usersRepository.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
